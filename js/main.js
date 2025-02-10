6459a1cb75fdb1b7a8502fd63e101da5e9add0f3
Mavlon4658 <mavlonbe4658>
2025-02-10 11:32:51 +0500

    ready

--- js/main.js
+++ js/main.js
@@ -3,38 +3,39 @@ const scrollBtns = document.getElementById('scrollBtns');
 
 // Skroll eventni tinglash
 window.addEventListener('scroll', () => {
-  if (window.innerWidth <= 765) { // Ekran kengligini tekshirish
-    if (window.scrollY > 100) {
-      scrollBtns.style.display = 'flex'; // 100px'dan keyin ko'rsatish
-    } else {
-      scrollBtns.style.display = 'none'; // 100px'dan oldin yashirish
+    if (scrollBtns) {
+        if (window.innerWidth <= 765) {
+            if (window.scrollY > 100) {
+                scrollBtns.style.display = 'flex';
+            } else {
+                scrollBtns.style.display = 'none';
+            }
+        } else {
+            scrollBtns.style.display = 'none';
+        }
     }
-  } else {
-    scrollBtns.style.display = 'none'; // 765px dan katta ekranlarda doim yashirish
-  }
 });
 
-// Sahifa yuklanganda ham tekshirish
 window.addEventListener('resize', () => {
-  if (window.innerWidth > 765) {
-    scrollBtns.style.display = 'none';
-  }
+    if (window.innerWidth > 765) {
+        if (scrollBtns) {
+            scrollBtns.style.display = 'none';
+        }
+    }
 });
 (() => {
     'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+
     const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
+
     Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
     })
-  })()
+})()
